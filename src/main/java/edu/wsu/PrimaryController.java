d64298aaed649c0471c950bc4bd60eaf5a5fe858
Ruth Kurniawati <rkurniawati@westfield.ma.edu>
2021-11-02 20:41:38 -0400

    from in class 11/1

--- src/main/java/edu/wsu/PrimaryController.java
+++ src/main/java/edu/wsu/PrimaryController.java
@@ -1,12 +1,30 @@
 package edu.wsu;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
+    TextField playerName;
+
+    @FXML
+    ComboBox<Integer> snakeLength;
+
+    @FXML
+    ColorPicker snakeColorPicker;
+
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    public void handleButtonAction(ActionEvent actionEvent) {
+        System.out.println(playerName.getText());
     }
 }
